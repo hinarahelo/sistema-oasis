1ab30beb6b71758874ad0f48535a73b6ba8ce833
The Mônia <hinarahelo@gmail.com>
2025-12-17 13:44:23 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const DISCORD_CLIENT_ID = "1450905109083979785";
+const DISCORD_CLIENT_SECRET = "yTuT6joTJ9nA28wRA3W-6EVkhJvN9il3";
+const REDIRECT_URI = "https://hinarahelo.github.io/sistema-oasis/callback.html"; // URL de callback do seu site
