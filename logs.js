4d5f44322ebe71637e193d3e7f0036ad02f37b79
The Mônia <hinarahelo@gmail.com>
2025-12-18 16:17:25 -0400

    Create logs.js

---
+++ logs.js
@@ -0,0 +1,12 @@
+import { addDoc, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export async function registrarLog(db, dados) {
+  try {
+    await addDoc(collection(db, "logs"), {
+      ...dados,
+      criadoEm: serverTimestamp()
+    });
+  } catch (e) {
+    console.warn("[LOG FALHOU]", e);
+  }
+}
