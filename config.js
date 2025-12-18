1d7ce729388be3f0b553669c8e86c05c767f21a4
The Mônia <hinarahelo@gmail.com>
2025-12-18 19:59:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+// Configuração OAuth Discord
 const DISCORD_CLIENT_ID = "1450905109083979785";
-const DISCORD_CLIENT_SECRET = "yTuT6joTJ9nA28wRA3W-6EVkhJvN9il3";
-const REDIRECT_URI = "https://hinarahelo.github.io/sistema-oasis/callback.html"; 
+const REDIRECT_URI = "https://hinarahelo.github.io/sistema-oasis/callback.html";
+
+export function discordAuthUrl() {
+  const params = new URLSearchParams({
+    client_id: DISCORD_CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: "code",
+    scope: "identify"
+  });
+  return "https://discord.com/api/oauth2/authorize?" + params.toString();
+}
